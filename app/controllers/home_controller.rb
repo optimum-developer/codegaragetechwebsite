be7d6a44abf0b76edf0c4e54546f5408eaba64ba
Paras Sharma <sharma.paras4444@gmail.com>
2017-10-10 14:17:03 +0530

    pushing code

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+	def index
+	end
+
+	def our_work
+	end
+
+	def about_us
+	end
+
+	def contact
+	end
+
+	def blog
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'home/about_us'=>'home#about_us'
+  get 'home/contact'=>'home#contact'
+  get 'home/our_work'=>'home#our_work'
+  get 'home/blog'=>'home#blog'
+
+end
