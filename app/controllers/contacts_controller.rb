55667fcfcf85d999e47e9861f54137bccb5f19ba
Paras Sharma <sharma.paras4444@gmail.com>
2017-11-17 12:13:22 +0530

    changes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,29 +6,20 @@ class ContactsController < ApplicationController
      		cont_email=@new_contact_us.email
      		cont_name=@new_contact_us.name
      		# MailUserMailer.send_mail_to_user(cont_email).deliver
-            MailUserMailer.send_mail_to_admin.deliver
+        MailUserMailer.send_mail_to_admin.deliver
 
-          respond_to do |format|
-                format.json { render :json =>{status: 200,status_notify:"success"}  }
-                format.html { }
-
-            end
+     		flash[:notice] = "successfull"
         else
-            respond_to do |format|
-                format.json { render :json =>{status: 200,status_notify:"unsuccess"}  }
-                format.html { }
-
-            end
-     		
+            flash[:notice]=@new_contact_us.errors.full_messages
         end
-        
-        # redirect_to :back
+
+        redirect_to :back
     end
 
     private
 	  def contact_params
 	    params.require(:contact).permit(:name,:email,:subject,:message)
 	  end
-    
-	
+
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+	# validates_format_of  :email, :with => /^[\+A-Z0-9\._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
+	# validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+	validates :email, presence: true
+	validates :name, presence: true
+	validates :subject, presence: true
+	validates :message, presence: true
 end
