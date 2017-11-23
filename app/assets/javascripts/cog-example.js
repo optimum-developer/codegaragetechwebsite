d39305ca071a0d0b86940913c302b5bef9cd91e4
Paras Sharma <sharma.paras4444@gmail.com>
2017-11-23 15:14:35 +0530

    cog error solved

--- app/assets/javascripts/cog-example.js
+++ app/assets/javascripts/cog-example.js
@@ -1,6 +1,6 @@
 jQuery(window).load(function($) {
 
-    $('.loader-icon').removeClass('spinning-cog').addClass('shrinking-cog');
-    $('.loader-background').delay(1300).fadeOut(); 
-     
+    jQuery('.loader-icon').removeClass('spinning-cog').addClass('shrinking-cog');
+    jQuery('.loader-background').delay(1300).fadeOut(); 
+
 });
