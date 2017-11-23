03fe8fa417c3c893ddb12405ac67aeadbcf1f2ef
Paras Sharma <sharma.paras4444@gmail.com>
2017-11-23 17:06:52 +0530

    blogs date

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,16 +4,16 @@ before_action :authenticate_admin!, only: [ :create, :new, :edit]
   def index
     @blogs = Blog.all.paginate(:page => params[:page], :per_page => 5)
   end
- 
+
   def show
     @blog = Blog.friendly.find(params[:id])
     @blogs=Blog.all.last(3)
   end
- 
+
   def new
     @blog = Blog.new
   end
- 
+
   def create
     @blog = Blog.new(blog_params)
     @blog.save
@@ -22,26 +22,27 @@ before_action :authenticate_admin!, only: [ :create, :new, :edit]
 
   def edit
     @blog = Blog.friendly.find(params[:id])
-  end 
+  end
   def update
     @blog = Blog.friendly.find(params[:id])
- 
+
     if @blog.update(blog_params)
       redirect_to @blog
     else
       render 'edit'
     end
   end
- 
+
     def destroy
       @blog = Blog.friendly.find(params[:id])
       @blog.destroy
-   
+
       redirect_to blogs_path
     end
-     
+
   private
     def blog_params
-      params.require(:blog).permit(:title, :text,:image)
+      debugger
+      params.require(:blog).permit(:title, :text,:image,:date_blog)
     end
 end
